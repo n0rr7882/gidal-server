a081e72065aafdbb6803126b1bde7545f2acbe93
N0rR <n0rr7882@gmail.com>
2018-03-31 20:26:46 +0900

    add process.env.port for Heroku

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 3000,
+    PORT: process.env.PORT || 3000,
     LOG_FORMAT: 'dev',
     DB_URL: 'mongodb://admin:Asdf!234@ds231229.mlab.com:31229/gidal-dev',
     SALT: 'ASDFGHJKL:"QWEROP{!@#()_'
